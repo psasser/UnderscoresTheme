6d40ba5112a771082be5fc63db6fc2db70d7e8ed
Patrick Sasser <patrickasasser@gmail.com>
2017-04-10 06:51:24 -0500

    Adding in Consistently Change Underscores Files

---
+++ header.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package _s
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+
+<?php wp_head(); ?>
+</head>
+<?php
+	$image = get_field('page_background', 'option');
+?>
+<body <?php body_class(); ?> style="background-image: url(<?php echo $image['url']; ?>)">
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', '_s' ); ?></a>
+
+	<header id="masthead" class="site-header" role="banner">
+		<?php themeHeader(); ?>
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
